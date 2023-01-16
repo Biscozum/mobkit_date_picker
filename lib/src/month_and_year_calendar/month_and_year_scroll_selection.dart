7fa98ee46638181bd2f4713f9811544569ce6850
Burakhan Gogce <burakhang@biscozum.com.tr>
2023-01-16 15:57:34 +0300

    pubspeb package name edited

--- lib/src/month_and_year_calendar/month_and_year_scroll_selection.dart
+++ lib/src/month_and_year_calendar/month_and_year_scroll_selection.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import '../../infact_calendar.dart';
+
+import '../../mobkit_date_picker.dart';
 
 class MonthAndYearScrollSelection extends StatefulWidget {
   final ValueNotifier<DateTime> calendarDate;
